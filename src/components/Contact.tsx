28560f59eef0f04b570759684f27f9002865398b
KezPersonal <kezbombdm@gmail.com>
2023-09-18 22:17:19 +0100

    links added

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,32 @@
+import { Box, Stack } from "@mui/material";
 import React from "react";
 import { FunctionComponent } from "react";
 
 const Contact: FunctionComponent = () => {
     return (
         <body>
-        <div>LinkedIn: https://www.linkedin.com/in/keelan-hassell-meng/</div>
-        <div>GitHub: https://github.com/PersonalKez</div>
+            <Stack direction={"row"} justifyContent={"center"}>
+                <Box padding={5}>
+                    <a href="https://www.linkedin.com/in/keelan-hassell-meng/">
+                        <Box
+                            component="img"
+                            alt="[LinkedIn Logo]"
+                            src={require("../LI-In-Bug.png")}
+                            sx={{ width: 50, height: 50, objectFit: "contain" }}
+                        />
+                    </a>
+                </Box>
+                <Box padding={5}>
+                    <a href="https://github.com/PersonalKez/">
+                        <Box
+                            component="img"
+                            alt="[Github Logo]"
+                            src={require("../github-mark.png")}
+                            sx={{ width: 50, height: 50, objectFit: "contain" }}
+                        />
+                    </a>
+                </Box>
+            </Stack>
         </body>
     )
 }
